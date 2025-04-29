2adeb9861edaa35ef0781f0526b6a49fa1aa566e
kiyieka <ehlertmoracha@gmail.com>
2025-04-29 10:23:54 +0300

    Add files via upload

---
+++ StudentProfile.java
@@ -0,0 +1,16 @@
+// Coding challenge 2
+// This is to show some of the data types availble.
+public class StudentProfile {
+    public static void main(String[] args) {
+        // Data types in use
+        String name = "John";     // text
+        int age = 20;              // whole number
+        double grade = 87.5;       // decimal
+
+
+        // Display student info
+        System.out.println("Student Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Grade: " + grade);
+    }
+}
